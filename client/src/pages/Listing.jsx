6fe6cf8a676b038ffac23595d62a9eb2339ebad7
Aynansh <aynansh2002@gmail.com>
2024-03-19 21:48:17 +0530

    minor tweaks

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -202,12 +202,10 @@ const Listing = () => {
               </AlertDescription>
             </Alert>
           )}
-          <div className="flex flex-col max-w-full mx-4 p-3 my-7 gap-3">
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-3">
             <p className="text-2xl font-semibold flex gap-4">
-              {listing.name} - ${" "}
-              {listing.offer
-                ? listing.discountedPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.name} - ₹{""}
+              {listing.regularPrice.toLocaleString("en-US")}
               {listing.type === "rent" && " / month"}
               {listingOwner === currentUser._id && (
                 <Link to={`/update-listing/${listing._id}`}>
@@ -234,14 +232,15 @@ const Listing = () => {
               </p>
               {listing.offer && (
                 <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                  ${+listing.regularPrice - +listing.discountedPrice} Discount
+                  ₹{+listing.regularPrice - +listing.discountedPrice} Discount
                 </p>
               )}
             </div>
-            <p className="text-slate-800 mb-2">
+            <div className="text-slate-800 mb-2">
               <span className="font-semibold text-black">Description - </span>
-              {listing.description}
-            </p>
+              <p className="text-justify hyphens-auto">{listing.description}</p>
+            </div>
+
             <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
               <li className="flex items-center gap-1 whitespace-nowrap ">
                 <FaBed className="text-lg" />
